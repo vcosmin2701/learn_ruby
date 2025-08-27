734e1f9e06718f34348bfc51f08b5d2bd053f8ad
vcosmin2701 <v.cosmin2717@gmail.com>
2025-08-27 16:46:09 +0300

    First implementation of calculator.rb, without extra-practice

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(num1, num2)
+  num1 + num2
+end
+
+def sum(numbers)
+  result = 0
+  numbers.each do |num|
+    result += num
+  end
+  result
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
